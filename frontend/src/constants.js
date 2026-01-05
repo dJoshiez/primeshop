52b5762f39c714c23d13942e6f2b14a97d50ddac
Dhruvvv Joshi <joshidhruv5657@gmail.com>
2026-01-05 21:05:17 +0530

    Fix production API base URL

--- frontend/src/constants.js
+++ frontend/src/constants.js
@@ -1,7 +1,10 @@
-// export const BASE_URL =
-//   process.env.NODE_ENV === 'develeopment' ? 'http://localhost:5000' : '';
-export const BASE_URL = ''; // If using proxy
-export const PRODUCTS_URL = '/api/products';
-export const USERS_URL = '/api/users';
-export const ORDERS_URL = '/api/orders';
-export const PAYPAL_URL = '/api/config/paypal';
+export const BASE_URL =
+  process.env.NODE_ENV === 'development'
+    ? ''
+    : 'https://primeshop-689f.onrender.com';
+
+// API endpoints
+export const PRODUCTS_URL = `${BASE_URL}/api/products`;
+export const USERS_URL = `${BASE_URL}/api/users`;
+export const ORDERS_URL = `${BASE_URL}/api/orders`;
+export const PAYPAL_URL = `${BASE_URL}/api/config/paypal`;
